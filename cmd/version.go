235d9fe8484fe36198a7ede360f34f031af85cee
alalax <alaxay8@gmail.com>
2026-03-04 22:10:16 +0300

    Update build info defaults

--- cmd/version.go
+++ cmd/version.go
@@ -2,16 +2,19 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	Version   = "0.1.0"
-	BuildDate = "04 Mar 2026"
+	Version   = "dev"
+	Commit    = "none"
+	BuildDate = "unknown"
 )
 
 func newVersionCmd() *cobra.Command {
+	initBuildInfoDefaults()
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print build version information",
@@ -19,5 +22,31 @@ func newVersionCmd() *cobra.Command {
 			fmt.Fprintf(cmd.OutOrStdout(), "version: %s\ncommit: %s\nbuildDate: %s\n", Version, Commit, BuildDate)
 			return nil
 		},
+	}
+}
+
+func initBuildInfoDefaults() {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	if Version == "dev" && bi.Main.Version != "" && bi.Main.Version != "(devel)" {
+		Version = bi.Main.Version
+	}
+	for _, s := range bi.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			if Commit == "none" && s.Value != "" {
+				if len(s.Value) > 7 {
+					Commit = s.Value[:7]
+				} else {
+					Commit = s.Value
+				}
+			}
+		case "vcs.time":
+			if BuildDate == "unknown" && s.Value != "" {
+				BuildDate = s.Value
+			}
+		}
 	}
 }
